78358edcf7d0cc10bd3fac34bc47b78abe843e51
guitarbien <guitarbien@gmail.com>
2021-04-24 21:50:54 +0800

    test

--- app/Commands/GenerateReleaseCommand.php
+++ app/Commands/GenerateReleaseCommand.php
@@ -34,8 +34,6 @@ class GenerateReleaseCommand extends Command
      */
     public function handle(Github $github): void
     {
-        dump($this->arguments());
-        return;
         try {
             $github->createRelease($this->argument('tag'), $this->argument('branch'), $this->argument('projectColumn'));
         } catch (Exception $e) {
