2c1a36de0dfe06c6c345da0b2b9bc09e78e48739
guitarbien <guitarbien@gmail.com>
2021-04-24 11:25:47 +0800

    input the branch name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -l
 
 echo "Hello $1 $2"
-time=$(date)
-echo "::set-output name=time::$time"
+php application releasing master "$1" "$2" "$3"
+
+#time=$(date)
+#echo "::set-output name=time::$time"
+
+echo "::set-output name=tag::$3"
